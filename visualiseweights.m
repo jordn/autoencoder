44aa5cb87ecb0f652d5339fc70c31d50f2652358
Jordan Burgess <jordanburgess@gmail.com>
2016-04-06 10:58:57 +0100

    Quicker weight visualisation.

--- visualiseweights.m
+++ visualiseweights.m
@@ -1,15 +1,17 @@
-function visualiseweights(w)
+function visualiseweights(W)
     % For now assuming 100 x 784
-    nNeurons = size(w,1); % This many images.
-    sideLength = sqrt(size(w,2));
-    figure(2);
-    for i = 1:nNeurons
-        subplot(sqrt(nNeurons), sqrt(nNeurons), i); 
-        imagesc(reshape(w(i,:), sideLength, sideLength));
-        axis tight;
-        axis off;
+    nNeurons = size(W,1); % This many images.
+    sideLength = sqrt(size(W,2)); 
+    gridLength = sqrt(nNeurons);
+    gridSize = [gridLength, gridLength];
+    im = nan(sideLength*gridLength);    
+    for n = 1:nNeurons
+        [i, j] = ind2sub(gridSize, n);
+        im((i-1)*sideLength+1:i*sideLength, (j-1)*sideLength+1:j*sideLength) ...
+            = reshape(W(n,:), sideLength, sideLength);
     end
-    
+    figure(2);
+    imagesc(im);
     colormap bone
 end
 
