79a5bb908255a7757e7ebd1ff909c5ff594ab39c
Jordan Burgess <jordanburgess@gmail.com>
2016-04-24 17:29:24 +0100

    Better visualisations

--- visualisereconstruction.m
+++ visualisereconstruction.m
@@ -1,11 +1,12 @@
-function visualiselayer(data, recon)
+function visualisereconstruction(data, recon)
     figure(1);
     sideLength = sqrt(length(data));
     subplot(1,2,1);
     imagesc(reshape(data, sideLength, sideLength));
+    title('Data');
     subplot(1,2,2);
     imagesc(reshape(recon, sideLength, sideLength));
     colormap bone;
-    title('Random reconstruction');
+    title('Reconstruction');
 end
 
