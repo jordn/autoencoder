98d00184f6b82b2b5a5570e8cf0aaaa47b5469b4
Jordan Burgess <jordanburgess@gmail.com>
2016-04-25 19:16:03 +0100

    Add PCA

--- run.m
+++ run.m
@@ -104,22 +104,3 @@ for i = 0:9
     axis off 
 end
 leg = legend('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Location','northwest')
-
-%% SVD
-
-hold off;
-[V, U] = svd(imagesCV');
-for i = 0:9  
-   v = V(labelsCV==i, 1:2);   
-    s{i+1} = scatter(v(:,1), v(:,2), 'filled', 'MarkerFaceColor', colors(i+1,:));
-    hold on;
-    pause(1)
-    axis off 
-end
-leg = legend('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Location', 'northwest');
-
-
-
-
-
-
--- runpca.m
+++ runpca.m
@@ -1,6 +1,6 @@
 %% PCA
 hold off;
-[V, U] = pca(imagesCV');
+[V, U] = pca(imagesCV', 'NumComponents', 2);
 for i = 0:9  
    v = U(labelsCV==i, 1:2);
    
@@ -11,6 +11,27 @@ for i = 0:9
 end
 
 legend('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+
+
+%% SVD
+
+
+hold off;
+[V, U] = svd(imagesCV');
+
+for i = 0:9  
+   v = V(labelsCV==i, 1:2);   
+    s{i+1} = scatter(v(:,1), v(:,2), 'filled', 'MarkerFaceColor', colors(i+1,:));
+    hold on;
+    pause(1)
+    axis off 
+end
+leg = legend('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Location', 'northwest');
+
+
+
+
 
 
 
