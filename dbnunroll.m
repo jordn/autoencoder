3d404cf38a2c60903e8c2d67766f59010d36e4b6
Jordan Burgess <jordanburgess@gmail.com>
2016-04-25 16:05:20 +0100

    Neural Net middle layer is linear

--- dbnunroll.m
+++ dbnunroll.m
@@ -1,16 +1,19 @@
 function nn = dbnunroll(dbn)
     % Unroll a DBN ("deep belief net" - stcakde RBMs) into a NN
     nn.sizes = [dbn.sizes fliplr(dbn.sizes(1:end-1))];
+    nRBMs = numel(dbn.rbm);
     for layer = 1 : numel(dbn.rbm)
         nn.rbm{layer} = dbn.rbm{layer};
         nn.rbm{layer}.deltaW = 0;
         nn.rbm{layer}.deltaB = 0;
     end
+    % From code layer to 'output'
     for layer = 1 : numel(dbn.rbm)
-        nn.rbm{numel(dbn.rbm) + layer}.W = dbn.rbm{numel(dbn.rbm) - layer + 1}.W';
-        nn.rbm{numel(dbn.rbm) + layer}.a = dbn.rbm{numel(dbn.rbm) - layer + 1}.b;
-        nn.rbm{numel(dbn.rbm) + layer}.b = dbn.rbm{numel(dbn.rbm) - layer + 1}.a;
-        nn.rbm{numel(dbn.rbm) + layer}.deltaW = 0;
-        nn.rbm{numel(dbn.rbm) + layer}.deltaB = 0;
+        nn.rbm{nRBMs + layer}.W = dbn.rbm{nRBMs - layer + 1}.W';
+        nn.rbm{nRBMs + layer}.a = dbn.rbm{nRBMs - layer + 1}.b;
+        nn.rbm{nRBMs + layer}.b = dbn.rbm{nRBMs - layer + 1}.a;
+        nn.rbm{nRBMs + layer}.deltaW = 0;
+        nn.rbm{nRBMs + layer}.deltaB = 0;
+        nn.rbm{nRBMs + layer}.hiddenUnits = 'logistic';
     end
 end
--- rbmup.m
+++ rbmup.m
@@ -1,3 +1,7 @@
 function y = rbmup(rbm, x)
-    y = sigmoid(rbm.W * x + repmat(rbm.b, 1, size(x, 2)));
+    if strcmp(rbm.hiddenUnits, 'linear')
+        y = rbm.W * x + repmat(rbm.b, 1, size(x, 2));
+    else
+        y = sigmoid(rbm.W * x + repmat(rbm.b, 1, size(x, 2)));
+    end
 end
